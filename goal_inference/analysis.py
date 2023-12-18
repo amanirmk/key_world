056d2edb45a4c50420ffc514d6d1a94ca08c4ca2
amanirmk <amainakilaas@hmc.edu>
2023-12-17 22:42:43 -0500

    graphs done

--- goal_inference/analysis.py
+++ goal_inference/analysis.py
@@ -8,13 +8,29 @@ from scipy.stats import pearsonr  # type: ignore[import-untyped]
 from goal_inference.game import Game
 
 
-def format_settings(update_criteria, alpha):
+def format_col(col):
+    if col == "alpha":
+        return r"$\alpha$"
+    return col.replace("_", " ").replace("avg", "average").title()
+
+
+def format_update(update_criteria):
+    if isinstance(update_criteria, str):
+        update_criteria = eval(update_criteria)
     kind, val = update_criteria
     if kind in ["action", "goal"]:
         c = "p"
     else:
         c = "n"
-    return rf"{kind}: ${c}={val}$, $\alpha={int(alpha)}$"
+    return rf"{kind}: ${c}={val}$"
+
+
+def format_alpha(alpha):
+    return rf"$\alpha={int(alpha)}$"
+
+
+def format_settings(update_criteria, alpha):
+    return rf"{format_update(update_criteria)}, {format_alpha(alpha)}"
 
 
 def extract_data(input_folder, mode):
@@ -28,14 +44,14 @@ def extract_data(input_folder, mode):
             update_criteria_str, alpha_str, idx_str = file.stem.split("_")
             df["beliefs"] = df["beliefs"].apply(eval)
             df["world"] = int(idx_str)
-            df["alpha"] = float(alpha_str.split("=")[1])
+            df["alpha"] = int(float(alpha_str.split("=")[1]))
             df["update_criteria"] = [eval(update_criteria_str.split("=")[1])] * len(
                 df.index
             )
         elif mode == "replay":
             df["subj"], idx, update_criteria_str, alpha_str = file.stem.split("_")
             df["world"] = int(idx)
-            df["alpha"] = float(alpha_str.split("=")[1])
+            df["alpha"] = int(float(alpha_str.split("=")[1]))
             df["update_criteria"] = [eval(update_criteria_str.split("=")[1])] * len(
                 df.index
             )
@@ -136,15 +152,33 @@ def get_comparison(data, groupby, avg_over=[]):
 
 def make_heatmaps(output_folder, summary, groupby, metric_cols):
     assert len(groupby) == 2
+    assert groupby[0] != "dummy"
+    summary["dummy"] = 1
     g = summary.groupby(groupby)
+    plt.rc("text", usetex=True)
     for col in metric_cols:
         m = g[col].mean().unstack(level=0)
-        plt.figure(figsize=(16, 4))
+        if groupby[1] == "dummy":
+            plt.figure(figsize=(16, 1))
+        else:
+            plt.figure(figsize=(16, 4))
         sns.heatmap(m, annot=True)
         if groupby[0] == "update_criteria":
             for y in [5, 10]:
                 plt.axvline(y, color="white", linewidth=5.0)
-        plt.title(col)
+            plt.gca().set_xticklabels(
+                [
+                    format_update(tick_label.get_text())
+                    for tick_label in plt.gca().get_xticklabels()
+                ]
+            )
+        plt.title(format_col(col))
+        plt.xlabel(format_col(groupby[0]))
+        plt.ylabel(format_col(groupby[1]) if groupby[1] != "dummy" else None)
+        if groupby[1] == "dummy":
+            plt.yticks([])
+        else:
+            plt.yticks(rotation=0)
         plt.savefig(
             output_folder / f"{'-'.join([col] + groupby)}.pdf", bbox_inches="tight"
         )
@@ -164,18 +198,18 @@ def make_belief_plots(output_folder, model_data):
         )["beliefs"]:
             goals = list(beliefs.iloc[0][0].keys())
             goal_data = [[b[g] for b in beliefs.iloc[0]] for g in goals]
-            plt.figure(figsize=(3, 3))
+            plt.figure(figsize=(3, 1.35))
             for g, g_data in zip(goals, goal_data):
                 plt.plot(
-                    g_data, color=(np.array(Game.key_colors(g)) / 255), linewidth=5
+                    g_data, color=(np.array(Game.key_colors(g)) / 255), linewidth=3
                 )
             plt.ylim((0, 1))
             plt.xticks([])
             plt.yticks([])
-            plt.title(format_settings(update_criteria, alpha))
             plt.savefig(
                 output_folder / f"{world}-{update_criteria}-{alpha}.pdf",
                 bbox_inches="tight",
+                pad_inches=0,
             )
             plt.close()
 
@@ -193,7 +227,7 @@ def analyze_data(
         make_heatmaps(
             output_folder,
             performance_summary,
-            groupby=["world", "solved"],
+            groupby=["world", "dummy"],
             metric_cols=["speed_ratio", "speed_diff", "solved"],
         )
 
@@ -209,6 +243,12 @@ def analyze_data(
             output_folder,
             performance_summary,
             groupby=["update_criteria", "alpha"],
+            metric_cols=["speed_ratio", "speed_diff", "solved"],
+        )
+        make_heatmaps(
+            output_folder,
+            performance_summary,
+            groupby=["world", "dummy"],
             metric_cols=["speed_ratio", "speed_diff", "solved"],
         )
         make_belief_plots(belief_plot_folder, data)
@@ -228,5 +268,7 @@ def analyze_data(
                 "avg_log_likelihood",
                 "goal_correlation_r",
                 "action_correlation_r",
+                "goal_correlation_p",
+                "action_correlation_p",
             ],
         )
